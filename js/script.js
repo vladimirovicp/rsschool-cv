34cf07e0f2e1dda85a2fdbdd4b04188257ecc8b0
vladimirovicp <vladimirovicp70@gmail.com>
2022-06-04 22:16:00 +0400

    feat: add mobile version

--- js/script.js
+++ js/script.js
@@ -1,37 +1,36 @@
 let menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+let menuLinksMob = document.querySelectorAll('.header__link[data-goto]');
 let sections = document.querySelectorAll('section');
+const menuIcon = document.querySelector('.menu__icon');
+
+
+    menuIcon.addEventListener('click', (event) => {
+        menuIcon.classList.toggle("open");
+        document.querySelector('.header__nav').classList.toggle('_active');
+        document.body.classList.toggle('_lock');
+    });
+
 
 if( menuLinks.length > 0){
     // console.log(menuLinks);
-
     menuLinks.forEach(menuLinks => {
         menuLinks.addEventListener("click", onMenuClick)
     });
 
     function onMenuClick(e){
         const menuLink = e.target;
-
-        // console.log(menuLink);
-
         menuLinks.forEach(menuLink => {
             if (menuLink.classList.contains('active')) {
                 menuLink.classList.remove('active');
             }
         });
-
         menuLink.classList.add('active');
-
-
-
         sections.forEach( value => {
             if (value.classList.contains('active'))
             {
                 value.classList.remove('active');
             }
         });
-
-        // console.log(menuLink);
-        // console.log(' | ' + menuLink.dataset.goto + ' | ' + document.querySelector('.' + menuLink.dataset.goto));
 
         if(menuLink.dataset.goto && document.querySelector('.' + menuLink.dataset.goto)){
             const gotoBlock = document.querySelector('.' + menuLink.dataset.goto);
@@ -45,9 +44,83 @@ if( menuLinks.length > 0){
             });
 
             e.preventDefault();
+        }
+    }
 
+
+
+
+}
+
+
+if( menuLinksMob.length > 0) {
+
+    menuLinksMob.forEach(menuLinks => {
+        menuLinks.addEventListener("click", onMenuMobClick)
+    });
+
+    function onMenuMobClick(e){
+        menuIcon.classList.toggle("open");
+        document.querySelector('.header__nav').classList.toggle('_active');
+        document.body.classList.toggle('_lock');
+
+        const menuLinkMob = e.target;
+        if(menuLinkMob.dataset.goto && document.querySelector('.' + menuLinkMob.dataset.goto)){
+            const gotoBlockMob = document.querySelector('.' + menuLinkMob.dataset.goto);
+            gotoBlockMob.classList.add('active');
+
+            // const gotoBlockValueMob = gotoBlockMob.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
+            const gotoBlockValueMob = gotoBlockMob.getBoundingClientRect().top + scrollY - 20;
+            window.scrollTo({
+                top: gotoBlockValueMob,
+                behavior: "smooth"
+            });
+
+            e.preventDefault();
         }
 
-
     }
+
 }
+
+// if( menuLinksMob.length > 0) {
+//     // console.log(menuLinks);
+//     menuLinksMob.forEach(menuLinks => {
+//         menuLinks.addEventListener("click", onMenuMobClick)
+//     });
+//
+//     function onMenuMobClick(e){
+//         document.body.classList.toggle('_lock');
+//         menuIcon.classList.toggle("open");
+//         document.querySelector('.header__nav').classList.toggle('_active');
+//
+//         const menuLinkMob = e.target;
+//         menuLinksMob.forEach(menuLink => {
+//             if (menuLinkMob.classList.contains('active')) {
+//                 menuLinkMob.classList.remove('active');
+//             }
+//         });
+//         menuLinkMob.classList.add('active');
+//         sections.forEach( value => {
+//             if (value.classList.contains('active'))
+//             {
+//                 value.classList.remove('active');
+//             }
+//         });
+//
+//         if(menuLinkMob.dataset.goto && document.querySelector('.' + menuLinkMob.dataset.goto)){
+//             const gotoBlockMob = document.querySelector('.' + menuLinkMob.dataset.goto);
+//             gotoBlockMob.classList.add('active');
+//
+//             const gotoBlockValueMob = gotoBlockMob.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
+//
+//             window.scrollTo({
+//                 top: gotoBlockValueMob,
+//                 behavior: "smooth"
+//             });
+//
+//             e.preventDefault();
+//         }
+//     }
+//
+// }
